564a413f16b832f204c1120defd7a1ae2d59ed95
code java <codejava2025@gmail.com>
2022-05-25 09:40:01 +0530

    aading updated files

---
+++ src/main/java/mystorehub/mystorehub/App.java
@@ -0,0 +1,13 @@
+package mystorehub.mystorehub;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
